62b7526bb55a218887c19e8141c9c61b1b2bc5e8
Danko Komlen <dkomlen@gmail.com>
2018-06-17 18:03:36 +0100

    [rust] improved nth-prime

--- rust/nth-prime/src/lib.rs
+++ rust/nth-prime/src/lib.rs
@@ -1,22 +1,18 @@
 pub fn nth(n: u32) -> Option<u32> {
-    let mut primes: Vec<u32> = vec![2,3,5,7,11,13,17,19,23];
-    if n < 1 {
-        None
-    } else {
-        let mut x = primes[primes.len() - 1];
-        while n as usize > primes.len() {
-            x += 1;
-            let mut is_prime = true;
-            for p in &primes[..primes.len() / 2] {
-                if x % p == 0 {
-                    is_prime = false;
-                    break;
+    let mut primes = vec![2,3,5,7,11,13,17,19,23];
+    match n {
+        x if x < 1 => None,
+        _ => {
+            let mut x = primes[primes.len() - 1];
+            while n as usize > primes.len() {
+                x += 2;
+                if primes.iter()
+                    .take_while(|p| **p < (x as f32).sqrt() as u32 + 1)
+                    .all(|p| x % p != 0) {
+                        primes.push(x);
                 }
             }
-            if is_prime {
-                primes.push(x);
-            }
+            Some(primes[n as usize -1])
         }
-        Some(primes[n as usize - 1])
     }
 }
