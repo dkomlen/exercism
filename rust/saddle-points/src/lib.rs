0ad44c1d4b1810a1e622ee9efec15034a478ae5e
Danko Komlen <dkomlen@gmail.com>
2018-08-24 15:04:53 +0100

    [rust] saddle points - revision 2

--- rust/saddle-points/src/lib.rs
+++ rust/saddle-points/src/lib.rs
@@ -1,35 +1,11 @@
-use std::cmp;
-use std::collections::HashSet;
-
 pub fn find_saddle_points(input: &[Vec<u64>]) -> Vec<(usize, usize)> {
-    let mut sol = Vec::new();
-    let mut max_indexes = HashSet::new();
-
-    for (r, row) in input.iter().enumerate() {
-        match row.iter().max() {
-            Some(max) => {
-                for (c, val) in row.iter().enumerate() {
-                    if val == max {
-                        max_indexes.insert((r,c));
-                    }
-                }
-            }
-            None => {}
-        }
+    if !input.is_empty() && !input[0].is_empty() {
+        let max_points = input.iter().enumerate().map(|(r, row)| (r, row.iter().max().unwrap())).collect::<Vec<_>>();
+        (0..input[0].len()).flat_map(|c| {
+            let min = input.iter().map(|row| row[c]).min().unwrap();
+            input.iter().enumerate().map(|(r, row)| (r, row[c])).filter(|(r, v)| *v == min && max_points.contains(&(*r,v))).map(|(r,_)| (r, c)).collect::<Vec<_>>()
+        }).collect::<Vec<_>>()
+    } else {
+        Vec::new()
     }
-
-    if !input.is_empty() {
-        for c in 0..input[0].len() {
-            let mut min = input[0][c];
-            for row in input {
-                min = cmp::min(min, row[c])
-            }
-            for (r, row) in input.iter().enumerate() {
-                if row[c] == min && max_indexes.contains(&(r,c)) {
-                    sol.push((r,c));
-                }
-            }
-        }
-    } 
-    return sol;
 }
