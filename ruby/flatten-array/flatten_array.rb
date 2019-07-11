9d43f2684953646d1268683c22c8aeba4eba524a
Danko Komlen <dkomlen@gmail.com>
2019-07-11 14:21:51 +0100

    Initial ruby exercises

---
+++ ruby/flatten-array/flatten_array.rb
@@ -0,0 +1,13 @@
+class FlattenArray
+    def self.flatten(xs)
+        ret = []
+        for x in xs
+            if x.respond_to?("count")
+                ret += flatten(x)
+            elsif x
+                ret += [x]
+            end
+        end
+        return ret
+    end
+end
