55160eb3febd412898290af9249bdf560f1b913a
Danko Komlen <dkomlen@gmail.com>
2019-09-01 18:07:51 +0100

    [ruby] Simplify matrix solution

--- ruby/matrix/matrix.rb
+++ ruby/matrix/matrix.rb
@@ -2,17 +2,9 @@ class Matrix
   attr_reader :rows, :columns
 
   def initialize(mat)
-    @rows = mat.split("\n").map do |row|
-      row.split.map do |num|
-        num.to_i
-      end
+    @rows = mat.each_line.map do |row|
+      row.split.map(&:to_i)
     end
-    n_cols = @rows[0]&.size || 0
-    @columns =  Array.new(n_cols){[]}
-    @rows.each do |row|
-      row.each.with_index do |item, col|
-        columns[col].append(item)
-      end
-    end
+    @columns = @rows.transpose
   end
 end
